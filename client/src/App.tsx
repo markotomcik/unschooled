ed122a3ad4b2d126439a79c057a75ccdfebfe60c
Marko Tomčík <markotomcik@gmail.com>
2021-06-07 20:50:59 +0200

    Design started

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -53,17 +53,23 @@ const App: FC = () => {
   }
 
   return (
-    <div className="flex flex-col max-w-xl m-auto justify-center p-4">
-      <div>
-        <div className="flex">
-          <input ref={todoNameRef} autoFocus type="text" className="flex-1 px-3 py-1 m-1 rounded-lg bg-green-400 font-medium text-green-900 placeholder-green-700 focus:outline-none focus:ring-4 focus:ring-green-400 focus:ring-opacity-50" placeholder="Add Task..." onKeyPress={handleKeyPress} />
-          <button className="px-3 py-1 m-1 rounded-lg bg-green-400 font-medium text-green-900 focus:outline-none focus:ring-4 focus:ring-green-400 focus:ring-opacity-50 active:bg-green-500" onClick={handleAddTask}>Add Task</button>
-          <button className="px-3 py-1 m-1 rounded-lg bg-green-400 font-medium text-green-900 focus:outline-none focus:ring-4 focus:ring-green-400 focus:ring-opacity-50 active:bg-green-500" onClick={handleClearTodos}>Clear Completed</button>
+    <>
+      <div className="min-h-screen w-100">
+        <div className="bg-yellow-100 py-16">
+          <div className="flex flex-col max-w-xl m-auto justify-center p-4 shadow-xl rounded-xl bg-yellow-200">
+            <div>
+              <div className="flex">
+                <input ref={todoNameRef} autoFocus type="text" className="flex-1 px-3 py-1 m-1 rounded-lg bg-yellow-500 font-medium text-yellow-900 placeholder-yellow-700 focus:outline-none focus:ring-4 focus:ring-yellow-500 focus:ring-opacity-50" placeholder="Add Task..." onKeyPress={handleKeyPress} />
+                <button className="px-3 py-1 m-1 rounded-lg bg-yellow-500 font-medium text-yellow-900 focus:outline-none focus:ring-4 focus:ring-yellow-500 focus:ring-opacity-50 active:bg-yellow-600" onClick={handleAddTask}>Add Task</button>
+                <button className="px-3 py-1 m-1 rounded-lg bg-yellow-500 font-medium text-yellow-900 focus:outline-none focus:ring-4 focus:ring-yellow-500 focus:ring-opacity-50 active:bg-yellow-600" onClick={handleClearTodos}>Clear Completed</button>
+              </div>
+              <div className="m-1 rounded-lg font-medium text-yellow-900">{todos.filter(todo => !todo.complete).length} left to do</div>
+            </div>
+            <TodoList todos={todos} toggleTodo={toggleTodo} clearTodo={clearTodo} />
+          </div>
         </div>
-        <div className="m-1 rounded-lg font-medium text-green-900">{todos.filter(todo => !todo.complete).length} left to do</div>
       </div>
-      <TodoList todos={todos} toggleTodo={toggleTodo} clearTodo={clearTodo} />
-    </div>
+    </>
   );
 }
 
